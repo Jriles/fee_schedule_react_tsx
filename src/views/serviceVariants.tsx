b53117a64db3857137507c228be1b98af5a130e1
Jack Riley <jriley9000@gmail.com>
2022-07-23 18:18:10 -0700

    service variant filter frontend work

--- src/views/serviceVariants.tsx
+++ src/views/serviceVariants.tsx
@@ -1,5 +1,5 @@
-import { Button, Col, Container, ListGroup, Pagination, Row, Table } from "react-bootstrap";
-import { DefaultApi, ServiceResponse, UpdateServiceSchema, VariantResponse } from '../components/api/api';
+import { Button, CloseButton, Col, Container, Dropdown, Form, ListGroup, Pagination, Row, Table } from "react-bootstrap";
+import { AttributeResponse, DefaultApi, ServiceResponse, UpdateServiceSchema, VariantResponse } from '../components/api/api';
 import { AxiosResponse } from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -11,11 +11,28 @@ interface ServiceVariantsProps {
 }
 
 export default function ServiceVariants(props:ServiceVariantsProps) {
-    const DELETE_SERVICE_MODAL_HEADER = "Delete Service?";
+    const DELETE_SERVICE_MODAL_HEADER = 'Delete Service?';
+    const SERVICE_FILTER_NAME = 'Service';
+    const SERVICE_FILTER_ID = 'serviceFilter';
     const [serviceVariants, setServiceVariants] = React.useState<VariantResponse[]>([]);
     const [pageNum, setPageNum] = React.useState(1);
-    const [serviceFilter, setServiceFilter] = React.useState();
-    const [attributeValsFilter, setAttributeValsFilter] = React.useState();
+
+    interface FilterOption {
+        name: string;
+        id: string;
+    }
+
+    interface Filter {
+        name: string;
+        options: FilterOption[]
+    }
+
+    interface FilterDict {
+        [id: string] : Filter
+    }
+
+    const [filters, setFilters] = React.useState<FilterDict>({});
+    const [selectedFilters, setSelectedFilters] = React.useState<FilterDict>({});
 
     React.useEffect(() => {
         props.feeScheduleApi.getVariants([],pageNum).then((response: AxiosResponse) => {
@@ -24,7 +41,43 @@ export default function ServiceVariants(props:ServiceVariantsProps) {
         .catch((error: any) => {
             console.log(error);
         });
-    }, [])
+
+        const fetchFilterData = async () => {
+            let filtersToLoad : FilterDict = {};
+            const serviceRes:any = await props.feeScheduleApi.getAllServices()
+            const serviceFilter : Filter = {
+                name: SERVICE_FILTER_NAME,
+                options: convertResourcesToFilterOptions(serviceRes.data.services)
+            }
+            filtersToLoad[SERVICE_FILTER_ID] = serviceFilter;
+
+            const attributesRes:any = await props.feeScheduleApi.getAllAttributes()
+            const attributes : any[] = attributesRes.data.attributes;
+            for (const attribute of attributes) {
+                const attributeValuesRes:any = await props.feeScheduleApi.getAllAttributeValues(attribute.id);
+                const attributeValues : any[] = attributeValuesRes.data.attribute_values
+                const attributeFilter : Filter = {
+                    name: attribute.title,
+                    options: convertResourcesToFilterOptions(attributeValues)
+                }
+                filtersToLoad[attribute.id] = attributeFilter
+            }
+            setFilters(filtersToLoad)
+        }
+
+        fetchFilterData()
+            .catch(console.error);
+    }, []) 
+
+    function convertResourcesToFilterOptions (resources:any) {
+        return resources.map(function(resource:any, i:number) {
+            const filterOption : FilterOption = {
+                name: resource.title,
+                id: resource.id,
+            }
+            return filterOption
+        })
+    }
 
     async function deleteServiceVariant(variantId:string) {
         try {
@@ -74,6 +127,58 @@ export default function ServiceVariants(props:ServiceVariantsProps) {
         });
     }
 
+    const RenderFilters = () => {
+        return Object.keys(filters).map(function(filterId, i) {
+            const filter = filters[filterId];
+            return (
+                <>
+                    <Dropdown.Item onClick={() => handleAddFilter(filterId)}>{filter.name}</Dropdown.Item>
+                </>
+            )
+        })
+    }
+
+    const RenderFilterOptions = (filterOptions:FilterOption[]) => {
+        return filterOptions.map(function(filterOption, i) {
+            return (
+                <>
+                    <option value={filterOption.id}>{filterOption.name}</option>
+                </>
+            )
+        })
+    }
+
+    const RenderSelectedFilters = () => {
+        return Object.keys(selectedFilters).map(function(filterId, i) {
+            const selectedFilter:Filter = selectedFilters[filterId];
+            return (
+                <Col sm={2}>
+                    <div>
+                        <p>{selectedFilter.name}</p>
+                        <CloseButton onClick={() => handleRemoveFilter(filterId)}/>
+                    </div>
+                    
+                    <Form.Select className="">
+                        {RenderFilterOptions(selectedFilter.options)}
+                    </Form.Select>
+                </Col>
+            )
+        })
+    }
+
+    function handleAddFilter(filterId:string) {
+        let newSelectedFilters : FilterDict = Object.assign({}, selectedFilters);
+        newSelectedFilters[filterId] = filters[filterId]
+        console.log(newSelectedFilters)
+        setSelectedFilters(newSelectedFilters);
+    }
+
+    function handleRemoveFilter(filterId:string) {
+        let newSelectedFilters : FilterDict = Object.assign({}, selectedFilters);
+        delete newSelectedFilters[filterId];
+        setSelectedFilters(newSelectedFilters);
+    }
+
     let items = [];
     for (let number = 1; number <= 5; number++) {
         items.push(
@@ -82,6 +187,9 @@ export default function ServiceVariants(props:ServiceVariantsProps) {
             </Pagination.Item>,
         );
     }
+
+    let SelectedFilters = RenderSelectedFilters();
+    const Filters = RenderFilters();
 
     return (
         <Container className="mt-5">
@@ -93,6 +201,25 @@ export default function ServiceVariants(props:ServiceVariantsProps) {
                     <Link to="create" className="text-white float-end" style={{ textDecoration: 'none' }} >
                         <Button className="mb-5">Create Service Variant</Button>
                     </Link>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col sm={10}>
+                    <Row className="ps-2">
+                        {SelectedFilters}
+                    </Row>
+                </Col>
+                <Col sm={2}>
+                    <Dropdown className="float-end mb-5">
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Add Filters
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            {Filters}
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Col>
             </Row>
 
